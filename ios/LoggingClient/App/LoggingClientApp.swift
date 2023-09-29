5fc6ce1a0a9382069c4ea2195c5645feaa7ac42b
Chuck Grindel <chuck.grindel@gmail.com>
2023-09-29 10:17:46 -0600

    Initial iOS app files

---
+++ ios/LoggingClient/App/LoggingClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoggingClientApp.swift
+//  LoggingClient
+//
+//  Created by Chuck Grindel on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LoggingClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
