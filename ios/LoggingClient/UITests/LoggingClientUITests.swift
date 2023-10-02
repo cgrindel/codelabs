bb361769f21d21610b44d68dcf1d935a66855015
Chuck Grindel <chuck.grindel@gmail.com>
2023-10-02 11:33:34 -0600

    Add helper to enter text into text field.

--- ios/LoggingClient/UITests/LoggingClientUITests.swift
+++ ios/LoggingClient/UITests/LoggingClientUITests.swift
@@ -15,12 +15,25 @@ final class LoggingClientUITests: XCTestCase {
         app.launch()
     }
 
-    func typeText(_ value: String, in textField: XCUIElement) {
-        textField.tap()
-        let keyboard = app.keyboards.element
-        XCTAssertTrue(keyboard.waitForExistence(timeout: 0.5))
-        textField.typeText(value)
-    }
+    // func typeText(_ value: String, in element: XCUIElement, replaceExisting: Bool) {
+    //     element.tap()
+    //     if replaceExisting {
+    //         guard let textField = element as? UITextField else {
+    //             fatalError("Expected a UITTextField.")
+    //         }
+    //         textField.selectedTextRange = textField.textRange(
+    //             from: textField.beginningOfDocument,
+    //             to: textField.endOfDocument
+    //         )
+    //     }
+    //     let keyboard = app.keyboards.element
+    //     XCTAssertTrue(keyboard.waitForExistence(timeout: 0.5))
+    //     element.typeText(value)
+    // }
+
+    // func typeText(_ value: String, in element: XCUIElement) {
+    //     typeText(value, in: element, replaceExisting: false)
+    // }
 
     func test_EditConnection_Cancel() throws {
         // Check the initial connection info
@@ -31,13 +44,11 @@ final class LoggingClientUITests: XCTestCase {
         // Launch the edit connection screen and tap cancel
         app.buttons["editConnectionButton"].tap()
         let host = app.textFields["hostTextField"]
-        // host.tap()
-        // host.typeText("123.1.1.2")
-        typeText("123.1.1.2", in: host)
+        host.enterText("123.1.1.2", app: app)
+        // typeText("123.1.1.2", in: host)
         let port = app.textFields["portTextField"]
-        // port.tap()
-        // port.typeText("123")
-        typeText("123", in: port)
+        // typeText("123", in: port)
+        port.enterText("123", app: app)
         let cancel = app.buttons["cancelConnectionButton"]
         cancel.tap()
         XCTAssertFalse(cancel.waitForExistence(timeout: 0.5))
@@ -72,5 +83,31 @@ final class LoggingClientUITests: XCTestCase {
 
     func testSendMessage() throws {
         XCTFail("IMPLEMENT ME!")
+    }
+}
+
+extension XCUIElement {
+    // Inspired by
+    // https://stackoverflow.com/questions/32821880/ui-test-deleting-text-in-text-field
+    func enterText(_ text: String, app: XCUIApplication, replaceExisting: Bool = true) {
+        guard let stringValue = value as? String else {
+            XCTFail("Attempted to enter text into element without a string value.")
+            return
+        }
+        // Get focus
+        tap()
+
+        // Wait for the keyboard to appear
+        let keyboard = app.keyboards.element
+        XCTAssertTrue(keyboard.waitForExistence(timeout: 0.5))
+
+        if replaceExisting {
+            let deleteString = String(
+                repeating: XCUIKeyboardKey.delete.rawValue,
+                count: stringValue.count
+            )
+            typeText(deleteString)
+        }
+        typeText(text)
     }
 }
